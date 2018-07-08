04b76e5c6d615dc1f206af31efe533a5490ac3e7
skgrange <skgrange@gmail.com>
2018-07-08 20:50:20 +0100

    :zap: enhance update builder so multiple variables can be used in where clauses

--- R/build_update_statements.R
+++ R/build_update_statements.R
@@ -4,36 +4,71 @@
 #' 
 #' @param df Input data frame to generate \code{UPDATE} statements with. 
 #' 
-#' @param where Which variable in \code{df} to be used for the \code{WHERE} 
+#' @param where Which variables in \code{df} to be used for the \code{WHERE} 
 #' clause. If not used, only a single row data frame can be used. 
+#' 
+#' @param squish Should whitespace around commas and equal signs be removed to
+#' make the SQL statement shorter? 
 #' 
 #' @author Stuart K. Grange
 #' 
-#' @return Invisible. 
+#' @return Invisible, a database table update. 
 #' 
 #' @export
-build_update_statements <- function(table, df, where = NA) {
+build_update_statements <- function(table, df, where = NA, squish = FALSE) {
   
   # Check inputs
   stopifnot(length(table) == 1)
-  stopifnot(length(where) == 1)
   
-  if (is.na(where) && nrow(df) != 1)
+  if (is.na(where[1]) && nrow(df) != 1)
     stop("If `where` is not used, input must have a single row...", call. = FALSE)
   
-  if (!is.na(where)) {
+  # Build where clauses
+  if (!is.na(where[1])) {
     
-    # Get where variable's values
-    values_where <- df[, where, drop = TRUE]
-    
-    # Quote if needed
-    if (is.character(values_where)) 
-      values_where <- str_c("'", values_where, "'")
-    
+    # Build where clause
+    sql_where <- build_sql_pairs(df[, where], sep = "and")
+    sql_where <- str_c(" WHERE ", sql_where)
+   
     # Drop where variable from data to be used for update statements
     df <- df[, setdiff(names(df), where)]
     
   }
+  
+  # Create sql pairs containing the data
+  sql_update <- build_sql_pairs(df, sep = ",")
+  
+  # Build update statement
+  sql_update <- str_c("UPDATE ", table, " SET ", sql_update)
+  
+  # Add where clauses
+  if (!is.na(where[1])) sql_update <- str_c(sql_update, sql_where)
+  
+  # Remove some whitespace to make statement smaller
+  if (squish) {
+    
+    sql_update <- str_replace_all(sql_update, ", ", ",")
+    sql_update <- str_replace_all(sql_update, " = ", "=")
+    
+  }
+  
+  return(sql_update)
+  
+}
+
+
+build_sql_pairs <- function(df, sep) {
+  
+  # Parse
+  stopifnot(length(sep) == 1)
+  sep <- stringr::str_trim(sep)
+  sep <- stringr::str_to_upper(sep)
+  
+  if (!sep %in% c(",", "AND"))
+    stop("`sep` must be one of 'AND' or ','...", call. = FALSE)
+  
+  sep <- ifelse(sep == "AND", " AND ", sep)
+  sep <- ifelse(sep == ",", ", ", sep)
   
   # Add quotes if character
   df <- dplyr::mutate_if(df, is.character, dplyr::funs(str_c("'", ., "'")))
@@ -46,17 +81,11 @@ build_update_statements <- function(table, df, where = NA) {
   names(keys) <- keys
   
   # Use keys and values to make sql pairs
-  df <- purrr::map2_dfr(keys, df, ~str_c(.x, " = ", .y))
+  df <- df %>% 
+    purrr::map2_dfr(keys, ., ~str_c(.x, " = ", .y)) %>% 
+    tidyr::unite(., "sql", 1:ncol(.), sep = sep) %>% 
+    pull()
   
-  # Unite all pairs together
-  df <- tidyr::unite(df, "united", 1:ncol(df), sep = ", ")
-  
-  # Build update statements
-  x <- str_c("UPDATE ", table, " SET ", pull(df))
-  
-  # Add where clauses
-  if (!is.na(where)) x <- str_c(x, " WHERE ", where, " = ", values_where)
-  
-  return(x)
+  return(df)
   
 }
