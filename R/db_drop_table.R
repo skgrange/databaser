17ba0e2f63a49fdf8fc4dfdcc923483b3da3befd
skgrange <s.k.grange@gmail.com>
2019-04-04 09:12:46 +0100

    :sparkles: enhance db_drop_table with cascade argument

--- R/db_drop_table.R
+++ R/db_drop_table.R
@@ -1,25 +1,33 @@
-#' Function to drop a table within a database. 
+#' Function to drop a table within a database.
 #' 
 #' @param con Database connection. 
+#' 
 #' @param table Table to drop. 
+#' 
+#' @param cascade Should the \code{DROP TABLE} statement also use a 
+#' \code{CASCADE} clause to force the table drop? 
 #' 
 #' @author Stuart K. Grange.
 #' 
-#' @return Invisible. 
+#' @return Invisible, \code{con}. 
 #' 
 #' @export
-db_drop_table <- function(con, table) {
+db_drop_table <- function(con, table, cascade = FALSE) {
 
   # Check if table exists
-  if (!db_table_exists(con, table)) 
-    stop("`table` does not exist in database...", call. = FALSE)
+  if (!db_table_exists(con, table)) {
+    stop("`table` does not exist in database...", call. = FALSE) 
+  }
   
   # Build statement
   sql <- stringr::str_c("DROP TABLE ", table)
   
-  # Do
+  # Add cascade to statement
+  if (cascade) sql <- stringr::str_c(sql, " CASCADE")
+  
+  # Drop
   db_execute(con, sql)
   
-  # No return
+  return(invisible(con))
   
 }
