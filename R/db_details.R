87a6e121a6b420c85bcee78a0bf99503729c680c
skgrange <s.k.grange@gmail.com>
2019-04-22 10:26:08 +0100

    :sparkles: add db_details and db_details_insert

---
+++ R/db_details.R
@@ -0,0 +1,73 @@
+#' Function to return some high level details about a database. 
+#' 
+#' @param con Database connection. 
+#' 
+#' @author Stuart K. Grange. 
+#' 
+#' @return Tibble with one observation. 
+#' 
+#' @examples
+#' \dontrun{
+#' 
+#' # Get some information about the database
+#' db_details(con)
+#' 
+#' }
+#' 
+#' @export
+db_details <- function(con) {
+  
+  # Used twice
+  tables <- db_list_tables(con)
+  
+  # Build tibble
+  tibble(
+    system = threadr::hostname(),
+    date = as.numeric(lubridate::now()),
+    class = db.class(con),
+    size = db_size(con, unit = "mb"),
+    table_count = length(tables),
+    table_names = stringr::str_c(tables, collapse = "; ")
+  )
+  
+}
+
+
+#' Function to return some high level details about a database. 
+#' 
+#' @param con Database connection. 
+#' 
+#' @param table Database table to insert details into. 
+#' 
+#' @param print Should \code{table} be printed post-insert? 
+#' 
+#' @author Stuart K. Grange. 
+#' 
+#' @return Invisible \code{con}. 
+#' 
+#' @examples
+#' \dontrun{
+#' 
+#' # Get some information about the database and insert into table
+#' db_details_insert(con)
+#' 
+#' }
+#' 
+#' @export
+db_details_insert <- function(con, table = "database_details", print = FALSE) {
+  
+  # Get details
+  df <- db_details(con)
+  
+  # Insert into database
+  db_insert(con, table, df, replace = FALSE)
+  
+  # Read and print table after insert
+  if (print) {
+    db_read_table(con, table) %>% 
+      print(n = Inf)
+  }
+  
+  return(invisible(con))
+  
+}
