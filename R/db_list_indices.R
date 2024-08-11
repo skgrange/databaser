3fc607e232f5266abe5b4eda2c4ca9b82486de57
skgrange <s.k.grange@gmail.com>
2024-08-11 15:24:25 +0200

    :sparkles: enhance db_list_indices to handle sqlite cases

--- R/db_list_indices.R
+++ R/db_list_indices.R
@@ -10,6 +10,7 @@ db_list_indices <- function(con) {
   if (db_class(con) == "sqlite") {
     df <- db_list_tables(con) %>% 
       purrr::map(~db_list_indices_worker(con, .x)) %>% 
+      purrr::keep(~nrow(.) != 0L) %>% 
       purrr::list_rbind()
   } else if (db_class(con) == "postgres") {
     df <- db_get(con, "SELECT * FROM pg_indexes")
@@ -26,13 +27,12 @@ db_list_indices <- function(con) {
 
 # For SQLite
 db_list_indices_worker <- function(con, table) {
-  
-  # Build statement
-  sql <- stringr::str_c("PRAGMA INDEX_LIST('", table, "')")
-  
-  # Query
-  df <- db_get(con, sql)
-  
-  return(df)
+
+  db_get(
+    con, 
+    stringr::str_glue(
+      "PRAGMA INDEX_LIST('{table}')"
+    )
+  )
   
 }
